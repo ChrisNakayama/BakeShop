7c824ae292450502655ba40bd1374517a4d9b29b
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-12 22:45:20 -0800

    Passing BreadConstructor test.

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -4,8 +4,13 @@ using Bakery.Models;
 namespace Bakery.Tests
 {
   [TestClass]
-public class BreadTests
+  public class BreadTests
   {
-
-}
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
 }
