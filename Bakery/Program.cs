c6c99d57c3c088d147119aa117d582a2bd0e8990
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-02-12 23:31:23 -0800

    Added Program.CS.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,7 +7,19 @@ namespace Bakery
   {
     public static void Main()
     {
-
+      Console.WriteLine("\r\nWelcome to Pierre's \r\nWe sell bread and pastries.\r\nSee our IG for today's seasonal varieties. \r\n \r\nRegardless of flavors prices are: \r\nBread: Buy 2 loaves, Get 1 Free \r\nPastry: Buy 1 for $2 or 3 for $5 \r\n \r\nHow many loaves of bread to order? [Simply hit enter without typing a number if you don't wish to order bread]");
+      string stringNumLoaves = Console.ReadLine();
+      int numLoaves = int.Parse(stringNumLoaves);
+      Bread customerBread = new Bread(numLoaves);
+      int breadSubTotal = customerBread.CalculateSubTotal();
+      Console.WriteLine("\r\nYour bread subtotal is $" + breadSubTotal + ".\r\n");
+      Console.WriteLine("\r\nHow many pastries to order? [Simply hit enter without typing a number if you don't wish to order pastries]");
+      string stringNumPastries = Console.ReadLine();
+      int numPastries = int.Parse(stringNumPastries);
+      Pastry customerPastry = new Pastry(numPastries);
+      int pastrySubTotal = customerPastry.CalculateSubTotal();
+      int orderTotal = breadSubTotal + pastrySubTotal;
+      Console.WriteLine("\r\nYour pastry subtotal is $" + pastrySubTotal + " and your order total is $" + orderTotal + ". Thank you for ordering with us!");
     }
   }
 }
